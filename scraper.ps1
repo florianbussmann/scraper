393dab1f085d59386f0086731bc12d9df0e0516a
Florian Bussmann <hallo@florian-bussmann.de>
2016-05-02 18:02:55 +0200

    Init powershell script

---
+++ scraper.ps1
@@ -0,0 +1,11 @@
+﻿$BASE_URL = "http://www.xkcd.com/"
+$LIMIT = 500
+
+$webClient = New-Object System.Net.WebClient
+$webClient.Encoding = [System.Text.Encoding]::UTF8
+$regex = [regex] '(?<=<title>)([\S\s]*?)(?=</title>)'
+
+for($i = 1; $i -le $LIMIT; $i++) {
+    $data = $webClient.downloadstring($BASE_URL+$i+"/")
+    [string] $i + ";" + $regex.Match($data).value.trim() | Out-File -Append scraped.txt
+}
