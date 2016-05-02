1473a10765115ab4e56cd3dfb283bc03705fc339
Florian Bussmann <hallo@florian-bussmann.de>
2016-05-02 18:10:35 +0200

    Continuus scraping

--- scraper.ps1
+++ scraper.ps1
@@ -6,6 +6,8 @@ $webClient.Encoding = [System.Text.Encoding]::UTF8
 $regex = [regex] '(?<=<title>)([\S\s]*?)(?=</title>)'
 
 for($i = 1; $i -le $LIMIT; $i++) {
-    $data = $webClient.downloadstring($BASE_URL+$i+"/")
-    [string] $i + ";" + $regex.Match($data).value.trim() | Out-File -Append scraped.txt
+    if((Get-Content scraped.txt | select-string -pattern "^$i;") -eq $null) {
+        $data = $webClient.downloadstring($BASE_URL+$i+"/")
+        [string] $i + ";" + $regex.Match($data).value.trim() | Out-File -Append scraped.txt
+    }
 }
